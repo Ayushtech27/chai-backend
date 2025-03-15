b71ccbc07b9578b29fd03208edd8d7b04f651131
Ayush Singhal <singhal.ayush27@gmail.com>
2025-03-16 04:08:45 +0530

    minor error correction

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { CORS_ORIGIN } from "./config";
+import { CORS_ORIGIN } from "../src/config/index.js";
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended, limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 

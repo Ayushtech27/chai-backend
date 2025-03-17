888e071d42d12ddbc951ef532770b4e8dc53b711
Ayush Singhal <singhal.ayush27@gmail.com>
2025-03-17 15:36:17 +0530

    all code setup

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { CORS_ORIGIN } from "../src/config/index.js";
+import routes from "./routes/index.js";
 
 const app = express();
 
@@ -16,5 +17,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+app.use("/api/v1", routes);
 
 export default app;
